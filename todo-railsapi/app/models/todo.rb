53d915100ad69d780b8d880f17dad7b393cb2d5e
Tirumarai Selvan <tirumarai.selvan@gmail.com>
2016-12-16 14:10:12 +0530

    init

---
+++ todo-railsapi/app/models/todo.rb
@@ -0,0 +1,9 @@
+
+class Todo < ActiveRecord::Base
+  scope :completed, -> { where("completed = ?", true) }
+  scope :active, -> { where("completed = ?", false) }
+
+  def title=(title)
+    write_attribute(:title, title.strip)
+  end
+end
