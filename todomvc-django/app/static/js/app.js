333adee8ea31dda442f2cc0985e325eba885391b
Anon Ray <rayanon004@gmail.com>
2016-12-23 15:04:17 +0530

    Disable CSRF for the todomvc-django

--- todomvc-django/app/static/js/app.js
+++ todomvc-django/app/static/js/app.js
@@ -5,18 +5,6 @@ var username = null;
 jQuery(function ($) {
   'use strict';
 
-  function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-  }
-  $.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-      if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-        xhr.setRequestHeader("X-CSRFToken", csrftoken);
-      }
-    }
-  });
-
   var setUsername = function (u) {
     username = u;
     $('#userinfo').text(username);
--- todomvc-django/app/todo/settings.py
+++ todomvc-django/app/todo/settings.py
@@ -92,9 +92,10 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'todo.middleware.DisableCSRF',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
