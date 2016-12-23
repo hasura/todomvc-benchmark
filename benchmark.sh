65df05a74c082aa73b40addfc1f19313cb4ad418
root <root@ubuntu-2gb-sfo1-01.localdomain>
2016-12-23 13:01:19 +0000

    post data

--- benchmark.sh
+++ benchmark.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/bash
+#!/bin/bash
 
 if [ -z "$1" ]
   then
@@ -17,12 +17,30 @@ SERVER=$1
 SESSION_COOKIE=$2
 
 declare -A AB_CMDS
-AB_CMDS=(
+AB_RAILS_CMDS=(
  [GET_ACCOUNT_INFO]="ab -k -q -c 20 -n 200 -C $SESSION_COOKIE $SERVER/user/account/info"
  [INSERT]="ab -k -q -c 20 -n 200 -p insert_data -T 'application/json' -C $SESSION_COOKIE $SERVER/api/v1/table/todo/insert"
  [UPDATE]="ab -k -q -c 20 -n 200 -p update_data -T 'application/json' -C $SESSION_COOKIE $SERVER/api/v1/table/todo/update"
  [UPDATE_ALL]="ab -k -q -c 20 -n 200 -p update_data -T 'application/json' -C $SESSION_COOKIE $SERVER/api/v1/table/todo/delete_completed"
 )
+
+
+#For django
+AB_DJANGO_CMDS=(
+ [GET_ACCOUNT_INFO]="ab -k -q -c 20 -n 200 -C $SESSION_COOKIE $SERVER/auth/user/"
+ [INSERT]="ab -k -q -c 20 -n 200 -p insert_data -T 'application/json' -C $SESSION_COOKIE $SERVER/api/tasks/"
+ [UPDATE]="ab -k -q -c 20 -n 200 -p update_data -m 'PATCH' -T 'application/json' -C $SESSION_COOKIE $SERVER/api/tasks/"
+ [UPDATE_ALL]="ab -k -q -c 20 -n 200 -p update_data -T 'application/json' -C $SESSION_COOKIE $SERVER/api/tasks/delete-completed"
+)
+
+#For hasura
+AB_CMDS=(
+ [GET_ACCOUNT_INFO]="ab -k -q -c 200 -n 2000 -C $SESSION_COOKIE $SERVER/user/account/info"	#Use auth server
+ [INSERT]="ab -k -q -c 200 -n 2000 -p insert_data -T 'application/json' -C $SESSION_COOKIE $SERVER/api/1/table/todo/insert"
+ [UPDATE]="ab -k -q -c 200 -n 2000 -p update_data -T 'application/json' -C $SESSION_COOKIE $SERVER/api/1/table/todo/update"
+ [UPDATE_ALL]="ab -k -q -c 200 -n 2000 -p delete_completed -T 'application/json' -C $SESSION_COOKIE $SERVER/api/1/table/todo/delete"
+)
+
 
 for test in "${!AB_CMDS[@]}"
 do
